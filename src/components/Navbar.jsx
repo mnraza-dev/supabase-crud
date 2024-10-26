86cef1eec81af9763d89e9545bbee3d98ae15a8d
mnraza-dev <mnraza94@gmail.com>
2024-10-26 16:28:37 +0530

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-     <h1>Supa Crud App</h1>
-     <Link to="/">Home</Link>
-     <Link to="/create">Create New</Link>
+    <nav className=" bg-black p-4 flex px-10 items-center justify-between">
+      <Link
+        className="hover:cursor-pointer hover:text-gray-300 text-white"
+        to="/"
+      >
+        Supa Crud App
+      </Link>
 
+      <div className="flex gap-4 text-sm text-white">
+        <Link className="hover:cursor-pointer hover:text-gray-300" to="/create">
+          Create New
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
