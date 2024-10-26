23d3dd680982fb0555d985123b636bc2a7a3200a
mnraza-dev <mnraza94@gmail.com>
2024-10-26 16:13:44 +0530

    added Home, Create and Update pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
